eb64298a89e40e7d22d0073189141ad47ad511b3
Mayank <mayank@wedigtech.com>
2018-12-26 11:39:51 +0530

    moring commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,9 @@
+import { HelpersService } from './../services/helpers/helpers.service';
 import { Component, OnInit,AfterViewInit } from '@angular/core';
 import {AuthService} from '../services/authService/auth.service';
 import { DataService } from '../services/data.service';
 import { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } from 'constants';
+
 declare var $: any;
 require('../../assets/js/owl.carousel.js');
 @Component({
@@ -11,12 +13,25 @@ require('../../assets/js/owl.carousel.js');
 })
 export class HeaderComponent implements OnInit,AfterViewInit {
 
-  constructor(public auth:AuthService,public dataservice:DataService) { }
+  constructor(public auth:AuthService,public dataservice:DataService,private helpers:HelpersService) { }
   dashboardDataApiUrl='users/getUserData'
   userName
   userEmail
   ngOnInit() {
-  
+    let accountToken = this.helpers.getLocalStoragData("accountToken"); // cehck if account token is exists
+    if (accountToken != null) {
+      // If not null
+      let accountTokenInfo = atob(accountToken).split(":"); // split token
+      console.log(accountTokenInfo);
+      if (accountTokenInfo[0] == this.auth.getCurrentUser().userId) {
+        // if token of current user
+       /*  if (parseInt(accountTokenInfo[2]) == 0) {
+          this.is_net_metering = false;
+        } else {
+          this.is_net_metering = true;
+        } */
+      }
+    }
   }
   ngAfterViewInit(){
     $(function() {
