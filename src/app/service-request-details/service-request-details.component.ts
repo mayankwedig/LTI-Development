be3645681e5ebf6e042782b8f3dc38d82a495daf
Mayank <mayank@wedigtech.com>
2018-12-29 20:27:24 +0530

    Final commit

--- src/app/service-request-details/service-request-details.component.ts
+++ src/app/service-request-details/service-request-details.component.ts
@@ -96,7 +96,7 @@ export class ServiceRequestDetailsComponent implements OnInit {
     });
   }
   getSerReqDtLoder: boolean = false;
-  serviceRequestDetails = [];
+  serviceRequestDetails:any= [];
   getServiceRequestDetails() {
     var requestData = {
       accountToken: btoa(this.accountNumber),
