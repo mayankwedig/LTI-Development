6d46b29ddccd6d420b8f7115f257bdd83f43bb0c
chandani-wedig <45990836+chandani-wedig@users.noreply.github.com>
2019-01-05 18:25:03 +0530

    changes

--- src/app/notification-list/notification-list.component.ts
+++ src/app/notification-list/notification-list.component.ts
@@ -34,36 +34,36 @@ export class NotificationListComponent implements OnInit {
   ngOnInit() {
 
 
-    this.getnotificationwithoutData();
+    //this.getnotificationwithoutData();
   };
 
-    getnotificationwithoutData() {
-      this.NotificationsLoader = true;
-      this.profile.getnotificationwithoutData().subscribe(
-        (response: any) => {
-          var res = response;
-          this.NotificationsLoader = false;
-          if (res.authCode) {
-            if (res.authCode == "200" && res.status == true) {
-              this.notificationsdata = res.data_params;
+    // getnotificationwithoutData() {
+    //   this.NotificationsLoader = true;
+    //   this.profile.getnotificationwithoutData().subscribe(
+    //     (response: any) => {
+    //       var res = response;
+    //       this.NotificationsLoader = false;
+    //       if (res.authCode) {
+    //         if (res.authCode == "200" && res.status == true) {
+    //           this.notificationsdata = res.data_params;
               
-              this.notificationfound = true;
-            } else {
-              this.notificationfound = false;
-              this.notificationsdata = "";
-            }
-          }
-        },
-        (error: AppError) => {
-          this.notificationfound = false;
-          this.NotificationsLoader = false;
-          if (error instanceof BadInput) {
-          } else {
-            throw error;
-          }
-        }
-      );
-    }
+    //           this.notificationfound = true;
+    //         } else {
+    //           this.notificationfound = false;
+    //           this.notificationsdata = "";
+    //         }
+    //       }
+    //     },
+    //     (error: AppError) => {
+    //       this.notificationfound = false;
+    //       this.NotificationsLoader = false;
+    //       if (error instanceof BadInput) {
+    //       } else {
+    //         throw error;
+    //       }
+    //     }
+    //   );
+    // }
 
 
   }
