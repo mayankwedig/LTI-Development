0ef3953f28007cfa39ed0635930c2c8bba9f21d1
Mayank <mayank@wedigtech.com>
2019-01-24 18:33:39 +0530

    comiit
    asas

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -13,9 +13,9 @@ import { promise } from 'protractor';
 @Injectable()
 export class DataService {
 //private apiUrl='http://103.249.98.101:3002/'; // Dev URL
- private apiUrl='http://103.249.98.246:3002/'; // Prod URL
+// private apiUrl='http://103.249.98.246:3002/'; // Prod URL
 //private apiUrl='http://192.168.1.156:3002/'; //local
-//private apiUrl='http://192.168.1.156:3003/'; //local soa
+private apiUrl='http://192.168.1.156:3003/'; //local soa
 
   constructor(private http: HttpClient) { }
 
